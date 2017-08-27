701cfc114cfeddb0a3421468fe5e6eec5558f618
Filip Ciepiela <filip.ciepiela@gmail.com>
2017-08-27 10:59:34 +0200

    removal of not used file.

--- manager/tests.py
+++ manager/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from manager.models import *
 
 # Create your tests here.
+"""
+Test should:
+    add user
+    login as user
+    upload file
+    create curve set
+    process file
+    check the result of processing
+"""
+
+class TestUser(TestCase):
+    def setUp(self):
+        user = User(name="U1")
+        user.save()
+
+    def test_user(self):
+        u = User.objects.all()
+        self.assertEqual(u[0].name, "U1")
+
+
