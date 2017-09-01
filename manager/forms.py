c98e5d896add95c537dc05d8fbf71f945606d272
Filip Ciepiela <filip.ciepiela@gmail.com>
2017-09-01 18:31:02 +0200

    requires changes to form processing [and graphical work]

--- manager/forms.py
+++ manager/forms.py
@@ -144,16 +144,61 @@ class SelectCurvesForCurveSetForm(forms.Form):
 
     def __init__(self, user,  *args, **kwargs):
         files = CurveFile.objects.filter(owner=user, deleted=False)
-        user_files_filter_qs = Q()
-        for f in files:
-            user_files_filter_qs = user_files_filter_qs | Q(curveFile=f)
-        user_curves = Curve.objects.filter(user_files_filter_qs)
 
         super(SelectCurvesForCurveSetForm, self).__init__(*args, **kwargs)
-        for cb in user_curves:
-            self.fields["curve_%d" % cb.id] = forms.BooleanField(
-                    label = cb.curveFile.name + ": " + cb.name + (" - %i" % cb.id),
-                    required = False )
+        order = ['name']
+
+        self.fields['FILES'] = forms.CharField(
+                label='',
+                initial='Files:')
+        self.fields['FILES'].widget.attrs['readonly'] = True
+        order.append('FILES')
+        for f in files:
+            fname = 'curveFile_{0}'.format(f.id)
+            self.fields[fname] = \
+                    forms.BooleanField(label=f,required=False)
+            self.fields[fname].widget.attrs['class'] = 'parent'
+            order.append(fname)
+            cf = Curve.objects.filter(curveFile=f)
+            for c in cf:
+                cname = "curve_{0}_curveFile_{1}".format(c.id,f.id)
+                self.fields[cname] = \
+                        forms.BooleanField(label = c, required = False )
+                self.fields[cname].widget.attrs['class'] = 'child'
+                order.append(cname)
+            self.fields['end_'+fname] = forms.CharField( label='', initial='')
+            self.fields['end_'+fname].widget.attrs['readonly'] = True
+            self.fields['end_'+fname].widget.attrs['class'] = 'invisible'
+            order.append('end_'+fname)
+
+        css = CurveSet.objects.filter(owner=user, deleted=False)
+        self.fields['CURVESETS'] = forms.CharField(
+                label='',
+                initial='Curve Sets:')
+        self.fields['CURVESETS'].widget.attrs['readonly'] = True
+        order.append('CURVESETS')
+        for cs in css:
+            csname = 'curveSet_{0}'.format(cs.id)
+            self.fields[csname] = \
+                    forms.BooleanField(
+                            label=cs,
+                            help_text='asdasdasd',
+                            required=False)
+            self.fields[csname].widget.attrs['class'] = 'parent'
+
+            order.append(csname)
+            for c in cs.usedCurveData.all():
+                cname = "curveData_{0}_curveSet_{1}".format(c.id,cs.id)
+                self.fields[cname] = \
+                        forms.BooleanField(label = c.curve, required = False )
+                self.fields[cname].widget.attrs['class'] = 'child'
+                order.append(cname)
+            self.fields['end_'+csname] = forms.CharField( label='', initial='')
+            self.fields['end_'+csname].widget.attrs['readonly'] = True
+            self.fields['end_'+csname].widget.attrs['class'] = 'invisible'
+            order.append('end_'+csname)
+        self.order_fields(order)
+
 
     def process(self, user):
         curves = []
