cb16f095e6563e78a102d7e3733cb3781c558ddd
Filip Ciepiela <filip.ciepiela@gmail.com>
2018-03-24 15:49:14 +0100

    PEP8ed methodmanager

--- manager/operations/methodmanager.py
+++ manager/operations/methodmanager.py
@@ -7,7 +7,6 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from django.http import HttpResponse
 from django.utils import timezone
-from django import forms
 from django.db import transaction
 import manager.models as mmodels
 import manager.plotmanager as pm
@@ -15,6 +14,8 @@ from manager.exceptions import VoltPyFailed
 from manager.helpers.functions import generate_plot
 from manager.helpers.functions import voltpy_render
 from manager.helpers.functions import add_notification
+from manager.exceptions import VoltPyDoesNotExists
+
 
 class MethodManager:
     """
@@ -39,25 +40,25 @@ class MethodManager:
 
         self.__loadMethods()
 
-        if ( 'curveset_id' in kwargs ):
+        if 'curveset_id' in kwargs:
             self.__type = 'other'
             self.__curveset_id = int(kwargs.get('curveset_id', None))
             return
-        elif ( 'processing_id' in kwargs  or 'processing' in kwargs ):
+        elif 'processing_id' in kwargs or 'processing' in kwargs:
             self.__type = 'processing'
             _id = int(kwargs.get('processing_id', kwargs.get('processing')))
             try:
                 self.__model = mmodels.Processing.objects.get(id=_id)
                 self.__curveset_id = self.__model.curveSet.id
             except ObjectDoesNotExist:
-                raise 404
-        elif ( 'analysis_id' in kwargs or 'analysis' in kwargs ):
+                raise VoltPyDoesNotExists
+        elif 'analysis_id' in kwargs or 'analysis' in kwargs:
             self.__type = 'analysis'
             _id = int(kwargs.get('analysis_id', kwargs.get('analysis')))
             try:
                 self.__model = mmodels.Analysis.objects.get(id=_id)
             except ObjectDoesNotExist:
-                raise 404
+                raise VoltPyDoesNotExists
         else:
             raise NameError('Unknown type')
         self.__activateMethod()
@@ -76,7 +77,7 @@ class MethodManager:
                 class_object = fimp.main_class
                 self.__registerMethod(class_object)
 
-    def __registerMethod(self,mclass):
+    def __registerMethod(self, mclass):
         if str(mclass.__name__) == self.methods[mclass.type()]:
             raise NameError("Name " + repr(mclass) + " already exists in " + mclass.type())
         self.methods[mclass.type()][str(mclass.__name__)] = mclass
@@ -87,41 +88,45 @@ class MethodManager:
             self.__method = mclass(self.__model)
 
     def process(self, request, user):
-        if ( request.method != 'POST' 
-        or request.POST.get('query') != 'methodmanager' ):
+        if any([
+            request.method != 'POST',
+            request.POST.get('query') != 'methodmanager'
+        ]):
             return
         if self.__method:
             try:
-                self.__method.process(user=user,request=request)
+                self.__method.process(user=user, request=request)
             except VoltPyFailed as e:
                 self.__model.deleted = True
                 self.__model.save()
                 add_notification(request, 'Procedure failed. The data may be incompatible with the processing method. Please verify and try again.')
                 add_notification(request, 'Fail reason: %s' % e)
 
-
     def exportFile(self):
         pass
 
     def getJSON(self, user):
         if not self.__method.has_next:
-            return { 'command': 'redirect', 'location': self.__model.getUrl(user)  }
+            return {'command': 'redirect', 'location': self.__model.getUrl(user)}
         else:
-            return { 'command': 'reload' }
+            return {'command': 'reload'}
 
     def getContent(self, request, user):
-        if self.__method.has_next == False or self.__method.step is None:
+        if any([
+            self.__method.has_next is False,
+            self.__method.step is None
+        ]):
             return HttpResponseRedirect(self.__model.getUrl(user))
         elif not self.isMethodSelected():
             return HttpResponseRedirect(reverse("browseCurveSet"))
 
         if self.__model.deleted:
             add_notification(request, 'Procedure delted.', 0)
-            return  HttpResponseRedirect(reverse("showCurveSet", args=[user.id, self.__model.curveSet.id]))
+            return HttpResponseRedirect(reverse("showCurveSet", args=[user.id, self.__model.curveSet.id]))
 
         stepText = dict( 
-            head= '', 
-            body= 'No text'
+            head='', 
+            body='No text'
         )
 
         if self.__method.step:
@@ -142,20 +147,20 @@ class MethodManager:
                 value_id=self.__model.curveSet.id,
                 vtype=self.__method.type(),
                 vid=self.__model.id,
-                interactionName = self.__method.step['class'].plot_interaction,
-                add = self.__method.getAddToPlot()
+                interactionName=self.__method.step['class'].plot_interaction,
+                add=self.__method.getAddToPlot()
             )
 
             context = {
                 'scripts': '\n'.join([ 
                                         plotScr, 
-                                        stepText.get('head','') 
+                                        stepText.get('head', '')
                                     ]),
                 'mainPlot': plotDiv,
                 'method_content': ''.join([
                                         step_numInfo,
-                                        stepText.get('desc',''),
-                                        stepText.get('body',''),
+                                        stepText.get('desc', ''),
+                                        stepText.get('body', ''),
                                     ]),
                 'user': user,
                 'model': self.__model,
@@ -170,9 +175,9 @@ class MethodManager:
 
     def getAnalysisSelectionForm(self, *args, **kwargs):
         return MethodManager.SelectionForm(
-            self, 
+            self,
             self.methods['analysis'],
-            type='analysis', 
+            type='analysis',
             prefix='analysis',
             *args, 
             **kwargs
@@ -195,7 +200,7 @@ class MethodManager:
             return ''
 
     def isMethodSelected(self):
-        return (self.__method != None)
+        return (self.__method is not None)
 
     class SelectionForm(forms.Form):
         """
@@ -204,9 +209,9 @@ class MethodManager:
         """
         def __init__(self,  parent, methods, *args, **kwargs):
             self.type = kwargs.pop('type', 'processing')
-            if ( self.type == 'processing' ):
+            if self.type == 'processing':
                 label = "Processing method"
-            elif (self.type == 'analysis' ):
+            elif self.type == 'analysis':
                 label = 'Analysis method'
             disabled = kwargs.pop('disabled', False)
             super(MethodManager.SelectionForm, self).__init__(*args, **kwargs)
@@ -214,8 +219,8 @@ class MethodManager:
             self.parent = parent
             choices = list(
                 zip(
-                    [ str(k) for k,v in methods.items() ],
-                    [ v.__str__() for k,v in methods.items() ]
+                    [str(k) for k, v in methods.items()],
+                    [v.__str__() for k, v in methods.items()]
                 )
             )
 
@@ -226,7 +231,7 @@ class MethodManager:
                 disabled=disabled
             )
             self.fields['method-description'] = forms.CharField(
-                widget=forms.Textarea(attrs={'readonly':'readonly'}),
+                widget=forms.Textarea(attrs={'readonly': 'readonly'}),
                 required=False,
                 initial=self.methods[list(self.methods)[0]].description,
                 label="Description:"
@@ -237,8 +242,8 @@ class MethodManager:
             js_dict = json.dumps(
                 dict(
                     zip(
-                        [ str(k) for k,v in self.methods.items() ],
-                        [ v.description for k,v in self.methods.items() ],
+                        [str(k) for k, v in self.methods.items()],
+                        [v.description for k, v in self.methods.items()],
                     )
                 )
             )
@@ -260,13 +265,13 @@ $(function(){{
             if self.type == 'processing':
                 if self.cleaned_data.get('method') in self.methods:
                     a = mmodels.Processing(
-                        owner = user,
-                        curveSet = curveset,
-                        method = self.cleaned_data.get('method'), 
-                        name = "",
-                        active_step_num = 0,
-                        deleted = False,
-                        completed = False
+                        owner=user,
+                        curveSet=curveset,
+                        method=self.cleaned_data.get('method'), 
+                        name="",
+                        active_step_num=0,
+                        deleted=False,
+                        completed=False
                     )
                     curveset.prepareUndo()
                     a.save()
@@ -276,20 +281,17 @@ $(function(){{
             elif self.type == 'analysis':
                 if self.cleaned_data.get('method') in self.methods:
                     a = mmodels.Analysis(
-                        owner = user,
-                        curveSet = curveset,
-                        method = self.cleaned_data.get('method'),
-                        name = "",
-                        active_step_num = 0,
-                        deleted = False,
-                        completed = False
+                        owner=user,
+                        curveSet=curveset,
+                        method=self.cleaned_data.get('method'),
+                        name="",
+                        active_step_num=0,
+                        deleted=False,
+                        completed=False
                     )
                     a.save()
-                    curveset.locked=True #CurveSet cannot be changed when used by Analysis method.
+                    curveset.locked = True  # CurveSet cannot be changed when used by Analysis method.
                     curveset.save()
                     return a.id
                 else:
                     return None
-
-if ( __name__ == '__main__' ):
-    mm = MethodManager()
