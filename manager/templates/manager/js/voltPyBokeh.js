73ebb20860a9b1160c21ca735117e9dbcfef05ff
Filip Ciepiela <filip.ciepiela@gmail.com>
2018-05-02 19:22:08 +0200

    Fix logic erro in plot size claculation

--- manager/templates/manager/js/voltPyBokeh.js
+++ manager/templates/manager/js/voltPyBokeh.js
@@ -5,13 +5,14 @@ function setMainPlotSize() {
   var winWidth = $(window).width();
   var w = Math.floor(winWidth/2.1);
   var h = Math.floor(w/proportion);
-  if (h<(winHeight*0.85)) {
+  if (h>(winHeight*0.85)) {
     w = Math.floor(h*0.85*proportion);
     h = Math.floor(w/proportion);
   }
   $('._voltPy_bokehSize').css('width', w);
   $('._voltPy_bokehSize').css('height', h);
   $('._voltPy_bokehButtonsSize').css('width', w);
+  window.dispatchEvent(new Event('resize'))
   
 }
 $(function(){
