7ce32808eebf6675972aa68d115c409781f7add0
Filip Ciepiela <filip.ciepiela@gmail.com>
2018-05-12 22:04:50 +0200

    Fix for UTF-8 displaying as emoji

--- manager/helpers/html.py
+++ manager/helpers/html.py
@@ -2,8 +2,8 @@ from django.utils.safestring import mark_safe
 
 
 def locked():
-    return mark_safe('<span style="margin: 0px; padding: 0px; color: red;">🔒</span>')
+    return mark_safe('<span style="margin: 0px; padding: 0px; color: red;">🔒&#xFE0E;</span>')
 
 
 def unlocked():
-    return mark_safe('<span style="margin: 0px; padding: 0px; color: green;">🔓</span>')
+    return mark_safe('<span style="margin: 0px; padding: 0px; color: green;">🔓&#xFE0E;</span>')
