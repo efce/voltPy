216d57b0186399158d1268521f5d9baa0bbda09a
Filip Ciepiela <filip.ciepiela@gmail.com>
2018-05-31 16:48:59 +0200

    Add logo

--- manager/templatetags/volt_html.py
+++ manager/templatetags/volt_html.py
@@ -11,3 +11,13 @@ def lock():
 @register.simple_tag
 def unlock():
     return unlocked()
+
+
+@register.simple_tag
+def topmenu_height():
+    return '71px'
+
+
+@register.simple_tag
+def topmenu_height_logo():
+    return '65px'
