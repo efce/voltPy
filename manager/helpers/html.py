0c41614737528bcf91c83a6f91cdd3d17d10559c
Filip Ciepiela <filip.ciepiela@gmail.com>
2018-07-04 13:03:39 +0200

    Change padlocks from chars to img's

--- manager/helpers/html.py
+++ manager/helpers/html.py
@@ -1,9 +1,14 @@
 from django.utils.safestring import mark_safe
+from django.contrib.staticfiles.templatetags.staticfiles import static
 
 
 def locked():
-    return mark_safe('<span style="margin: 0px; padding: 0px; color: red;">🔒&#xFE0E;</span>')
+    return mark_safe('<img src="%s" alt="locked" style="border:0px; margin: 0px; padding: 0px"/>' % ( 
+        static('manager/padlock_close.png')
+    ))
 
 
 def unlocked():
-    return mark_safe('<span style="margin: 0px; padding: 0px; color: green;">🔓&#xFE0E;</span>')
+    return mark_safe('<img src="%s" alt="locked" style="border:0px; margin: 0px; padding: 0px"/>' % ( 
+        static('manager/padlock_open.png')
+    ))
